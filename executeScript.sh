4cacd616f0a7add39a73cd8f60957a4af299452e
ITS-V-I-J-A-Y-B-0-R-A-T-E <vijborate9@gmail.com>
2023-10-09 20:33:52 +0530

    LATEST COMMIT

--- executeScript.sh
+++ executeScript.sh
@@ -1,8 +1,1 @@
-echo "The bulit will be updated in every 1 minute as you commit on github"
-echo "& executeScript.sh will be runned" 
-echo  "BULID 2 Chnages---->"
-echo "BUILD 2 COMPLETED**"
-echo "BUILD 3 STARTED**"
-pwd
-date
-echo "BUILD 2 COMPLETED**"
+ echo "hello world"
