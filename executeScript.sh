23eb0e8ae2d08c91e57e6e39328167ecb347b92d
ITS-V-I-J-A-Y-B-0-R-A-T-E <vijborate9@gmail.com>
2023-10-09 19:41:44 +0530

    Commit 2

--- executeScript.sh
+++ executeScript.sh
@@ -1,2 +1,6 @@
 echo "The bulit will be updated in every 1 minute as you commit on github"
 echo "& executeScript.sh will be runned" 
+echo  "BULID 2 Chnages---->"
+pwd
+date
+echo "BUILD 2 COMPLETED**"
