4d3cb69e1f9f3a0ffb7c695df4103da24b0ff9a9
VBCoder <142000589+ITS-V-I-J-A-Y-B-0-R-A-T-E@users.noreply.github.com>
2023-10-14 19:20:40 +0530

    Update tomcat9-Installscript.sh

--- tomcat9-Installscript.sh
+++ tomcat9-Installscript.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 echo "Tomcat script started"
-TOMCAT_VERSION="9.0.80"
-DOWNLOAD_DIR="/home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"  #  directory where you want to download Tomcat
-EXTRACT_DIR="/home/ncs/Downloads/TOMCATINSTALLATION/ExtractingTomcatFiles"  #  directory where you want to extract Tomcat
+TOMCAT_VERSION="9.0.82"
+DOWNLOAD_DIR="/home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
+EXTRACT_DIR="/home/ncs/Downloads/TOMCATINSTALLATION/ExtractingTomcatFiles"
 
-echo "Creating folder-> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles "
+echo "Creating folder-> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 # Create the download directory if it doesn't exist
-mkdir -p $DOWNLOAD_DIR
+sudo mkdir -p $DOWNLOAD_DIR
 if [ $? -eq 0 ]; then
     echo "DOWNLOAD DIRECTORY CREATED --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 else
     echo "****Cannot create folder --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 fi
 
-# Create the extract directory if it doesn't exis
-mkdir -p $EXTRACT_DIR
+# Create the extract directory if it doesn't exist
+sudo mkdir -p $EXTRACT_DIR
 if [ $? -eq 0 ]; then
-    echo "EXTARCTING DIRECTORY CREATED --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
+    echo "EXTRACTING DIRECTORY CREATED --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 else
-    echo "****Cannot create EXTRACTING  folder --> /home/ncs/Downloads/TOMCATINSTALLATION/ExtractingTomcatFiles"
+    echo "****Cannot create EXTRACTING folder --> /home/ncs/Downloads/TOMCATINSTALLATION/ExtractingTomcatFiles"
 fi
 
 # Download Tomcat to the specified directory
-wget -P $DOWNLOAD_DIR https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+sudo wget -P $DOWNLOAD_DIR https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 if [ $? -eq 0 ]; then
-    echo "Download TOMCAT SUCCESFULL AT  --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
+    echo "Download TOMCAT SUCCESSFUL AT  --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 else
     echo "****DOWNLOAD FAILED"
 fi
 
 # Extract Tomcat to the specified directory
-tar -zxvf ${DOWNLOAD_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C $EXTRACT_DIR
+sudo tar -zxvf ${DOWNLOAD_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C $EXTRACT_DIR
 if [ $? -eq 0 ]; then
-    echo "extracting   TOMCAT SUCCESFULL AT  --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
+    echo "Extracting TOMCAT SUCCESSFUL AT  --> /home/ncs/Downloads/TOMCATINSTALLATION/Jenkins/DownloadTomcatFiles"
 else
-    echo "****EXTARCTION FAILED"
+    echo "****EXTRACTION FAILED"
 fi
 
 # Start Tomcat
-${EXTRACT_DIR}/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
+sudo ${EXTRACT_DIR}/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
 if [ $? -eq 0 ]; then
-    echo "starting tomcat SUCCESSSS"
+    echo "Starting tomcat SUCCESS"
 else
-    echo "****starting tomcat FAILED"
+    echo "****Starting tomcat FAILED"
 fi
-
