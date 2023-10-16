a6afca9a608d1e0e25cf4579a74f6116e45f4fd7
VBCoder <142000589+ITS-V-I-J-A-Y-B-0-R-A-T-E@users.noreply.github.com>
2023-10-16 17:11:55 +0530

    Update tomcat-Installscript.sh

--- tomcat-Installscript.sh
+++ tomcat-Installscript.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-set -e
-
-TOMCAT_VERSION="9.0.82"
-DOWNLOAD_DIR="/home/vijay/bashprogramming/Jenkins/DownloadTomcatFiles"
-EXTRACT_DIR="/home/vijay/bashprogramming/Jenkins/ExtractingTomcatFiles"
+TOMCAT_VERSION="9.0.80"
+DOWNLOAD_DIR="/home/vijay/bashprogramming/Jenkins/DownloadTomcatFiles"  #  directory where you want to download Tomcat
+EXTRACT_DIR="/home/vijay/bashprogramming/Jenkins/ExtractingTomcatFiles"  #  directory where you want to extract Tomcat
 
 # Create the download directory if it doesn't exist
-sudo mkdir -p "$DOWNLOAD_DIR"
-echo "Download directory created or already exists."
+mkdir -p $DOWNLOAD_DIR
 
 # Create the extract directory if it doesn't exist
-sudo mkdir -p "$EXTRACT_DIR"
-echo "Extraction directory created or already exists."
+mkdir -p $EXTRACT_DIR
 
 # Download Tomcat to the specified directory
-sudo wget -P "$DOWNLOAD_DIR" "https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
-echo "Tomcat downloaded successfully."
+wget -P $DOWNLOAD_DIR https://downloads.apache.org/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
 # Extract Tomcat to the specified directory
-sudo tar -zxvf "${DOWNLOAD_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz" -C "$EXTRACT_DIR"
-echo "Tomcat extracted successfully."
+tar -zxvf ${DOWNLOAD_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C $EXTRACT_DIR
 
 # Start Tomcat
-sudo "${EXTRACT_DIR}/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh"
-echo "Tomcat started successfully."
+${EXTRACT_DIR}/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
